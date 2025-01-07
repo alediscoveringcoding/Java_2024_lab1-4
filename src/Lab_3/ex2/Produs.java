46db893f83bb220ec9cccda6e79c5552172941dd
alediscoveringcoding <alexandra.mita@student.upt.ro>
2025-01-07 23:43:20 +0200

    rezolvare eroare lab 4

--- src/Lab_3/ex2/Produs.java
+++ src/Lab_3/ex2/Produs.java
@@ -8,39 +8,39 @@ public class Produs {
     private int cantitate;
     private LocalDate data;
 
-    public Produs(String denumire, double pret, int cantitate, LocalDate date) {
+    public Produs(String denumire, double pret, int cantitate, LocalDate data) {
         this.denumire = denumire;
         this.pret = pret;
-        this.cantitate=cantitate;
-        this.data=date;
+        this.cantitate = cantitate;
+        this.data = data;
     }
 
     public String getDenumire() {
         return denumire;
     }
 
-    public double getPret() {
-        return pret;
-    }
-
-    public int getCantitate() {
-        return cantitate;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
     public void setDenumire(String denumire) {
         this.denumire = denumire;
+    }
+
+    public double getPret() {
+        return pret;
     }
 
     public void setPret(double pret) {
         this.pret = pret;
     }
 
+    public int getCantitate() {
+        return cantitate;
+    }
+
     public void setCantitate(int cantitate) {
         this.cantitate = cantitate;
+    }
+
+    public LocalDate getData() {
+        return data;
     }
 
     public void setData(LocalDate data) {
@@ -55,5 +55,14 @@ public class Produs {
                 ", cantitate=" + cantitate +
                 ", data=" + data +
                 '}';
+    }
+
+    public void vindeProdus(int cantitate) {
+        if (this.cantitate >= cantitate) {
+            this.cantitate -= cantitate;
+            System.out.println(cantitate + denumire + " au fost vandute");
+        } else {
+            System.out.println("nu mai e pe stoc " + this.cantitate);
+        }
     }
 }
